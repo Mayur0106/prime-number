1c0a2820dd0910bdc47020e2a7e2b5148a60b474
Mayur  Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-07-29 00:37:48 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,32 @@
 import java.util.Scanner;
-public class Main{
-public static void main(String [] args)
-{
-    Scanner gh=new Scanner(system.in);
-    
-    int num=gh.nextInt();
-    
-    bool checker=false;
+public class Main {
 
-}
+    public static void main(String[] args) {
+        Scanner gh = new Scanner(System.in);
+
+        System.out.println("Enter the Number : ");
+        int num = gh.nextInt();
+
+        boolean checker = false;
+        if(num==1)
+        {
+            System.out.println("1 is not prime nor even number : ");
+        }
+        else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    break;
+                } else {
+                    checker = true;
+                }
+            }
+
+
+            if (checker) {
+                System.out.printf("The Given Number %d is prime ", num);
+            } else {
+                System.out.printf("The Given Number %d is not prime ", num);
+            }
+        }
+    }
 }
