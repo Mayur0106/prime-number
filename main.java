2a0a2a4bc3cdab2ddc0eda5e288546518e14ad00
Mayur  Shinde <97580421+Mayur0106@users.noreply.github.com>
2022-07-29 00:39:51 +0530

    Update main.java

--- main.java
+++ main.java
@@ -16,7 +16,7 @@ public class Main {
             for (int i = 2; i < num; i++) {
                 if (num % i == 0) {
                     break;
-                } else {
+                } else  {
                     checker = true ;
                 }
             }
